c71841920b79c04897729f119feb839af6b70b0d
ashak299 <ashak299@users.noreply.github.com>
2020-08-13 07:14:02 +0100

    adding webcam and file uploader

--- GUI_Flask/HelloFlask/views.py
+++ GUI_Flask/HelloFlask/views.py
@@ -1,7 +1,14 @@
 from flask import Flask
 from HelloFlask import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return "Hello Flask!"
+
+    return render_template(
+        "index.html",
+        title = "Hello Flask",
+        message = "Encrypting and Decrypting files, using face recognition",
+        extensionMessage = "Files to be encrypted must have extension: .txt, .docx or .doc",
+        select = "Select a file: ")
