1a607d25ad71385816622c7591f611e04bbd8536
ashak299 <ashak299@users.noreply.github.com>
2020-08-17 08:14:00 +0100

    upload file functionality

--- GUI_Flask/HelloFlask/views.py
+++ GUI_Flask/HelloFlask/views.py
@@ -1,15 +1,53 @@
-from flask import Flask
+import flask
+from flask import Flask, render_template, request, redirect, redirect
+import flask_uploads
+from flask_uploads import UploadSet, DATA
+import os
 import HelloFlask
 from HelloFlask import app
-from flask import render_template
+import werkzeug
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = 'HelloFlask/static/file_uploads/'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 @app.route('/home')
 def home():
-
     return render_template(
         "index.html",
         title = "Hello Flask",
         message = "Encrypting and Decrypting files, using face recognition",
-        extensionMessage = "Files to be encrypted must have extension: .txt, .docx or .doc",
-        select = "Select a file: ")
+        extensionMessage = "Firstly, use the webcam to take a snapshot of your face")
+
+@app.route('/FileUpload')
+def FileUpload():
+    return render_template(
+        "FileUpload.html",
+        title= "Upload File",
+        message= "Upload File to Encrypt",
+        select= "Select a file to upload")
+
+
+#file=UploadSet('file', DATA, default_dest=lambda x:'static/file_uploads/')
+
+# Upload API
+@app.route('/uploader', methods=['GET', 'POST'])
+def uploader():
+    if request.method == 'POST':
+        profile = request.files['file']
+        profile.save(os.path.join(UPLOAD_FOLDER, secure_filename(profile.filename)))
+        return redirect('http://localhost:5555/')
+    return redirect('http://localhost:5555/NotUploaded')
+
+
+
+@app.route('/NotUploaded')
+def NotUploaded():
+    return render_template(
+        "NotUploaded.html")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- GUI_Flask/runserver.py
+++ GUI_Flask/runserver.py
@@ -5,8 +5,10 @@ if __name__ == '__main__':
     HOST = os.environ.get('SERVER_HOST', 'localhost')
 
     try:
-        PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        #PORT = int(os.environ.get('SERVER_PORT', '5555'))
+        PORT = 5555
     except ValueError:
         PORT = 5555
 
+    app.debug=True
     app.run(HOST, PORT)
