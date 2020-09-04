933e961e2ae66915d0cf08d8a892621edd37982d
ashak299 <ashak299@users.noreply.github.com>
2020-09-04 19:20:57 +0100

    -error handling code, for incorrect face images -Deleting files after decryption

--- GUI_Flask/HelloFlask/views.py
+++ GUI_Flask/HelloFlask/views.py
@@ -1,5 +1,5 @@
 import flask
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, send_from_directory
 import flask_uploads
 from flask_uploads import UploadSet, DATA
 import os
@@ -23,7 +23,9 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
 
 #FUNCTIONS
+#reed solomon coder
 RS=rs.RSCoder(128,90)
+
 #takes in a numpy ndarray and outputs a 32 byte value
 def randIntTo32(x):
     #padding with 0s to reach 256 int values
@@ -52,7 +54,7 @@ def encrypt(key):
     #Initalisation Vector
     iv = os.urandom(16)
     #saving iv
-    ivFile=open("HelloFlask/static/encryption_folder/ivFile", 'ab')
+    ivFile=open("HelloFlask/static/encryption_folder/ivFile", 'wb')
     pickle.dump(iv, ivFile)
     ivFile.close()
 
@@ -116,20 +118,26 @@ def areEqual(arr1, arr2, n):
 
 #Function used to extract feature vectors and binarize
 def features(path):
-    image = face_recognition.load_image_file(path)
-    #extracting feature vectors
-    encoding = face_recognition.face_encodings(image)[0];
-    #binarization
-    arr=np.array(encoding)
-    arr=np.where(arr>np.mean(arr), 1, 0);
+    try:
+        image = face_recognition.load_image_file(path)
+        #extracting feature vectors
+        encoding = face_recognition.face_encodings(image)[0];
+        #binarization
+        arr=np.array(encoding)
+        arr=np.where(arr>np.mean(arr), 1, 0);
 
-    return arr;
+        return arr;
+    #no face detected
+    except IndexError:
+        res=['no', 'face', 'detected']
+        return res;
 
 
 #Generating Lock function and creates random key, then calls encrypt function
 def createLock(path):
     #turning image into binarized feature vector
     A1=features(path);
+    print(A1)
 
     #generating random Key
     randKey=np.random.choice([0, 1], size=(90,), p=[1./2, 1./2])
@@ -154,7 +162,6 @@ def check(secondFacePath, randomKey, lock):
     try:
         #turning image into binarized feature vector
         A2=features(secondFacePath)
-        print(A2)
 
         #xor lock with second feature vector
         Temp= A2 ^ lock
@@ -187,12 +194,13 @@ def check(secondFacePath, randomKey, lock):
             return False;
     #this exception is thrown when the face is not a match
     except rs.RSCodecError:
-        print("Not a match, over threshold", flush=True)
+        print("Not a match, over threshold: i.e result if the two faces are from different people", flush=True)
         return False;
 
 
 #------------------------------------------------------------------------------------
 
+#APP ROUTING
 #decoding image from base 64 and saving it to file path
 def getI420FromBase64(codec, path):
     base64_data = re.sub('^data:image/.+;base64,', '', codec)
@@ -222,6 +230,12 @@ def home():
 #file uploader page
 @app.route('/FileUpload')
 def FileUpload():
+    #checking image contains a face before proceeding
+    img=features("HelloFlask/static/image_uploads/first_image.jpeg")
+    if (isinstance(img[0], str)):
+        return redirect("http://localhost:5555/NotUploaded")
+
+    #uploading file if image is appropriate
     return render_template(
         "FileUpload.html",
         title= "Upload File",
@@ -243,7 +257,9 @@ def uploader():
 @app.route('/NotUploaded')
 def NotUploaded():
     return render_template(
-        "NotUploaded.html")
+        "NotUploaded.html",
+        errorMsg = "Face not detected in image",
+        explanation= "Make sure all your face is clearly shown")
 
 
 #uploading face image
@@ -251,12 +267,12 @@ def NotUploaded():
 def ImageUpload():
     if request.method == 'POST':
         faceImage=request.form.get('face');
-        getI420FromBase64(faceImage, "HelloFlask/static/image_uploads/first_image.jpeg");
+        img=getI420FromBase64(faceImage, "HelloFlask/static/image_uploads/first_image.jpeg");
         return redirect('http://localhost:5555/FileUpload')
     return redirect('http://localhost:5555/NotUploaded')
 
 
-#Creating Lock
+#Creating Biometric Lock used in encryption /decryption and encrypts file
 @app.route('/CreateLock')
 def CreateLock():
     
@@ -264,23 +280,21 @@ def CreateLock():
     #using this random key to encrypt the file
     lock,key=createLock("HelloFlask/static/image_uploads/first_image.jpeg");
 
-    print(key, flush=True)
-
     #Saving lock
-    f=open("HelloFlask/static/encryption_folder/lock", 'ab')
+    f=open("HelloFlask/static/encryption_folder/lock", 'wb')
     pickle.dump(lock, f)
     f.close()
 
     #saving key
-    f1=open("HelloFlask/static/encryption_folder/randomKey", "ab")
+    f1=open("HelloFlask/static/encryption_folder/randomKey", "wb")
     pickle.dump(key, f1)
     f1.close();
 
     return render_template(
-        "CreateLock.html")
+        "CreateLock.html",
+        encrypted = "File Encrypted")
 
-
-#redirect to web cam page 2
+#upload second image
 @app.route('/SecondImage', methods=['GET', 'POST'])
 def SecondImage():
     if request.method == 'POST':
@@ -290,46 +304,76 @@ def SecondImage():
     return redirect('http://localhost:5555/NotUploaded')
 
 
+#checks if second image  matches first -> if a match, decrypts file
 @app.route('/checking')
 def checking():
-    #get random key
-    keyFile= open("HelloFlask/static/encryption_folder/randomKey", 'rb')
-    original_key = pickle.load(keyFile)
-    keyFile.close();
-    #print(original_key,flush=True)
-    #print(type(original_key), flush=True)
+    #check if image contains a face before proceeding.
+    img=features("HelloFlask/static/image_uploads/second_image.jpeg")
+    #no face detected
+    if (isinstance(img[0], str)):
+        return redirect("http://localhost:5555/NotUploadedDecrypt")
 
-
-    #get lock
-    lockFile=open("HelloFlask/static/encryption_folder/lock", 'rb')
-    biometric_lock=pickle.load(lockFile)
-    lockFile.close();
-    #print(biometric_lock, flush=True)
-
-    #check if two values are equal, file is decrypted
-    isSuccess=check("HelloFlask/static/image_uploads/second_image.jpeg", original_key, biometric_lock)
-
-    #redirecting
-    if isSuccess:
-        #clearing files
-        os.remove("HelloFlask/static/image_uploads/second_image.jpeg")
-        os.remove("HelloFlask/static/image_uploads/first_image.jpeg")
-        os.remove("HelloFlask/static/encryption_folder/ivFile")
-        os.remove("HelloFlask/static/encryption_folder/lock")
-        os.remove("HelloFlask/static/encryption_folder/randomKey")
-        os.remove("HelloFlask/static/encryption_folder/EncryptedFile.enc")
-
-        return render_template(
-            "checking.html")
     else:
-        return redirect('http://localhost:5555/NotSuccessful')
+        #get random key
+        keyFile= open("HelloFlask/static/encryption_folder/randomKey", 'rb')
+        original_key = pickle.load(keyFile)
+        keyFile.close();
+        #print(original_key,flush=True)
+        #print(type(original_key), flush=True)
 
 
+        #get lock
+        lockFile=open("HelloFlask/static/encryption_folder/lock", 'rb')
+        biometric_lock=pickle.load(lockFile)
+        lockFile.close();
+        #print(biometric_lock, flush=True)
+
+        #check if two values are equal -> if equal, file is decrypted
+        isSuccess=check("HelloFlask/static/image_uploads/second_image.jpeg", original_key, biometric_lock)
+
+        if (isSuccess):
+            print("SUCCESS", flush=True)
+            #screen allows user to download decrypted file
+            return render_template(
+                "checking.html",
+                success= "Success!")
+        else:
+            return redirect('http://localhost:5555/NotSuccessful')
+
+#no face detected in second image
+@app.route('/NotUploadedDecrypt')
+def NotUploadedDecrypt():
+    return render_template(
+        "NotUploadedDecrypt.html",
+        errorMsg= "No Face Detected in this image",
+        explantion = "Make sure all your face is visible")
+
+#redirects here if not a match
 @app.route('/NotSuccessful')
 def NotSuccessful():
     return render_template(
-        "NotSuccessful.html")
+        "NotSuccessful.html",
+        no_match= "Face was not a match, decryption not successful")
+
+#clearing files
+@app.route('/deleteFiles')
+def deleteFiles():
+        #clearing files
+        os.remove("HelloFlask/static/image_uploads/first_image.jpeg")
+        os.remove("HelloFlask/static/image_uploads/second_image.jpeg")
+        os.remove("HelloFlask/static/encryption_folder/EncryptedFile.enc")
+        os.remove("HelloFlask/static/encryption_folder/ivFile")
+        os.remove("HelloFlask/static/encryption_folder/lock")
+        os.remove("HelloFlask/static/encryption_folder/randomKey")
+        os.remove("HelloFlask/static/encryption_folder/DecryptedFile.txt")
+
+        file=os.listdir("HelloFlask/static/file_uploads")[0]
+        filePath=os.path.join("HelloFlask/static/file_uploads", file)
+        os.remove(filePath)
+
+        return redirect('http://localhost:5555/')
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
